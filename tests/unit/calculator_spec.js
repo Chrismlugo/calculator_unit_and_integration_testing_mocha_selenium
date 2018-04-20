cd701dfb01ce27af93dd0916f4b9af5ac3acc9e1
Chris Murphy <_chrism_@live.com>
2018-04-20 11:08:32 +0100

    writes unit  tests can divide, all numbers can be clicked and can clear

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -44,6 +44,41 @@ describe('calculator', function () {
     assert.equal(2000, calculator.runningTotal);
   })
 
+  it('can divide', function(){
+    calculator.clearClick();
+    calculator.numberClick(9);
+    calculator.operatorClick("/");
+    calculator.numberClick(4);
+    calculator.operatorClick("=");
+    assert.equal(2.25, calculator.runningTotal);
+
+  })
+
+  it('all numbers can be clicked', function(){
+    calculator.clearClick();
+    calculator.numberClick(0);
+    calculator.numberClick(1);
+    calculator.numberClick(2);
+    calculator.numberClick(3);
+    calculator.numberClick(4);
+    calculator.numberClick(5);
+    calculator.numberClick(6);
+    calculator.numberClick(7);
+    calculator.numberClick(8);
+    calculator.numberClick(9);
+    assert.equal(0123456789, calculator.runningTotal);
+  })
+
+  it('can clear', function(){
+    calculator.numberClick(2);
+    calculator.numberClick(4);
+    calculator.numberClick(6);
+    calculator.clearClick();
+    assert.equal(0, calculator.runningTotal);
+  })
+
+
+
 
 
 
